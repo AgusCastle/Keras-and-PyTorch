cfc5d1247255f8b83cbf8d120a75317f0ed34e89
Agus Castillo <agu13xd@hotmail.com>
2022-04-13 16:10:16 -0500

    Update

--- Capitulo 9/main.py
+++ Capitulo 9/main.py
@@ -1,4 +1,21 @@
+import matplotlib.pyplot as plt
 from deep_net_keras import Net_Keras
+
+#       (** Eje X **) (** Eje Y **) Esta solo es una prueba 
+# -- Debemos pasar todos los resultados 
+# a listas o tuplas para poder generar la grafica
+
+# Este es un ejemplo nadamas
+fig, ax = plt.subplots()
+dias = ['L', 'M', 'X', 'J', 'V', 'S', 'D'] # Estas serian nuestras epocas
+temperaturas = {'Madrid':[28.5, 30.5, 31, 30, 28, 27.5, 30.5], # valores de accuracy
+                'Barcelona':[24.5, 25.5, 26.5, 25, 26.5, 24.5, 25]} # Valores loss
+#
+ax.plot(dias, temperaturas['Madrid'], linestyle = 'dashed')
+ax.plot(dias, temperaturas['Barcelona'], linestyle = 'dotted')
+plt.show()
+
+# Este ejemplo ya tenemos todo
 
 net_keras = Net_Keras()
 
